dbe78758096e65ad7882bfe6c63b9a944d96eecf
kevinfang2 <kevkev0123@gmail.com>
2017-07-29 16:39:21 -0700

    image array into commitable text file

---
+++ same.py
@@ -0,0 +1,22 @@
+import cv2
+import sys
+import os
+
+def image_to_text(image_array, file_name):
+    git_logs = open(file_name, 'w')
+
+    git_logs.write(str(len(image_array[0])) + "\n")
+    git_logs.write(str(len(image_array)) + "\n")
+
+    for row in image_array:
+        for char in row:
+            # print (type(char))
+            for color in char:
+                git_logs.write(str(color) + " ")
+            # git_logs.write(str(char))
+            git_logs.write("\n")
+    git_logs.close()
+
+image_array = cv2.imread("randomimages/kevin.png")
+
+image_to_text(image_array, "hallo.txt")
